cea1da8a4c4fcce492cc709d64a4567d42e488a3
Anurag Nimkande <anurag.nimkande@gmail.com>
2025-03-12 19:45:32 +0530

    new files added

--- alumni_site/alumni_site/settings.py
+++ alumni_site/alumni_site/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'alumni_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'projects',
+        'CLIENT': {
+            'host': 'mongodb+srv://anuragnimkande:anurag123@cluster0.6sft9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+        }
     }
 }
 
