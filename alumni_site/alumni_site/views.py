830ef6064ab994e0d602165955b70e084258f6a5
Anurag Nimkande <anurag.nimkande@gmail.com>
2025-03-23 14:57:08 +0530

    new interface changes

--- alumni_site/alumni_site/views.py
+++ alumni_site/alumni_site/views.py
@@ -7,17 +7,28 @@ def index(request):
 
 def login(request):
     if request.method == "POST":
-        email = request.POST.get("email")
+        email = request.POST.get("username")
         password = request.POST.get("password")
 
+        print(email,password)
+
         try:
+            
             user = Login.objects.get(email=email)  # Check if user exists
+            print(user)
             if user.password == password:  # Check password (not secure, use hashing instead)
                 messages.success(request, "Login successful!")
-                return redirect("home")  # Redirect to a homepage or dashboard
+                print("Success")
+                return redirect("admindash")  # Redirect to a homepage or dashboard
             else:
                 messages.error(request, "Invalid password")
         except Login.DoesNotExist:
             messages.error(request, "User does not exist")
+        
+        for user in Login.objects.all():
+            print(f"Email: {user.email}, Name: {user.name}, Password: {user.password}")
 
     return render(request, "login.html")
+
+def admindash(request):
+    return render(request,"admindash.html")
